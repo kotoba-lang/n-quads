30e101ff3373af703417deb4292bc055e8b2c1bd
Jun Kawasaki <jun784@gmail.com>
2026-07-20 23:51:43 +0900

    Migrate N-Quads serializer to Kotoba

---
+++ test/n_quads_test.clj
@@ -0,0 +1,67 @@
+(ns n-quads-test
+  (:require [clojure.java.io :as io]
+            [clojure.java.shell :as shell]
+            [clojure.test :refer [deftest is testing]]
+            [kotoba.compiler.core :as compiler]
+            [kotoba.compiler.ir :as ir]))
+
+(def source (slurp "src/n_quads.kotoba"))
+(defn call [kir function & args] (ir/execute kir function (vec args)))
+(defn dstr [value] ["string" value])
+(defn dkw [value] ["keyword" value])
+(defn dmap [entries]
+  ["map" (->> entries (sort-by (comp str key)) (mapv (fn [[key value]] [key value])))])
+(defn dvec [& values] ["vector" (vec values)])
+(defn iri [value] (dmap {:rdf/type (dkw :iri) :value (dstr value)}))
+(defn blank [value] (dmap {:id (dstr value) :rdf/type (dkw :blank)}))
+(defn literal
+  ([value] (dmap {:rdf/type (dkw :literal) :value (dstr value)}))
+  ([value language] (dmap {:language (dstr language) :rdf/type (dkw :literal) :value (dstr value)})))
+(defn quad-value [subject predicate object graph]
+  (dmap (cond-> {:object object :predicate predicate :subject subject} graph (assoc :graph graph))))
+
+(deftest reference-preserves-n-quads-serialization-and-rejection
+  (let [kir (:kir (compiler/compile-source source :js-kotoba-v1))
+        first-quad (quad-value (iri "s") (iri "p") (literal "o\"\n" "en") (iri "g"))
+        second-quad (quad-value (blank "b0") (iri "p") (literal "v") nil)]
+    (is (= "a\\\\b\\\"c\\nd\\re" (call kir 'esc "a\\b\"c\nd\re")))
+    (is (= "<s> <p> \"o\\\"\\n\"@en <g> ." (call kir 'quad first-quad)))
+    (is (= "<s> <p> \"o\\\"\\n\"@en <g> .\n_:b0 <p> \"v\" .\n"
+           (call kir 'n-quads (dvec first-quad second-quad))))
+    (testing "unknown and malformed terms fail closed"
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (call kir 'term (dmap {:rdf/type (dkw :unknown)}))))
+      (is (thrown? clojure.lang.ExceptionInfo
+                   (call kir 'term (dmap {:rdf/type (dkw :iri)})))))
+    (is (= #{} (set (:effects kir))))))
+
+(defn compiler-root []
+  (nth (iterate #(.getParent ^java.nio.file.Path %)
+                (java.nio.file.Path/of (.toURI (io/resource "kotoba/compiler/core.clj")))) 4))
+(defn base64 [value] (.encodeToString (java.util.Base64/getEncoder) value))
+
+(deftest restricted-javascript-and-typed-wasm-have-semantic-conformance
+  (let [javascript (compiler/compile-source source :js-kotoba-v1)
+        wasm (compiler/compile-source source :wasm32-browser-kotoba-v1)
+        js64 (base64 (.getBytes ^String (:source javascript) "UTF-8"))
+        wasm64 (base64 ^bytes (:bytes wasm))
+        probe (shell/sh
+               "node" "--input-type=module" "-e"
+               (str "import(process.argv[1]).then(async host=>{"
+                    "const j=await import('data:text/javascript;base64," js64 "');"
+                    "const w=await host.instantiateKotoba(Buffer.from(process.argv[2],'base64'));"
+                    "const iri=v=>['map',[[':rdf/type',['keyword',':iri']],[':value',['string',v]]]];"
+                    "const lit=v=>['map',[[':rdf/type',['keyword',':literal']],[':value',['string',v]]]];"
+                    "const q=['map',[[':object',lit('o')],[':predicate',iri('p')],[':subject',iri('s')]]];"
+                    "const qs=['vector',[q]];const bad=['map',[[':rdf/type',['keyword',':unknown']]]];"
+                    "const run=(x,doc)=>{if(x.quad(doc(q))!=='<s> <p> \\\"o\\\" .')throw Error('quad');"
+                    "if(x['n-quads'](doc(qs))!=='<s> <p> \\\"o\\\" .\\n')throw Error('sequence');"
+                    "let rejected=false;try{x.term(doc(bad))}catch(e){rejected=true}if(!rejected)throw Error('reject');};"
+                    "run(j.instantiateKotoba({}),x=>x);run(w.instance.exports,w.typedValues.document);"
+                    "}).catch(e=>{console.error(e);process.exit(99)})")
+               (.toString (.toUri (.resolve (compiler-root) "runtime/browser-host.mjs"))) wasm64)]
+    (is (zero? (:exit probe)) (:err probe))))
+
+(deftest production-source-authority
+  (is (= ["src/n_quads.kotoba"]
+         (->> (file-seq (io/file "src")) (filter #(.isFile %)) (map str) sort vec))))
